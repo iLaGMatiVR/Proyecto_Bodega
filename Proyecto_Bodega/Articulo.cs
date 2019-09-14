c9ad0af2bebca2f6ef7316ef52224cdb5be14cf9
KalilKevin <kalilmaldonado.km@gmail.com>
2019-09-14 19:05:01 -0400

    Agregando 2 constructores

--- Proyecto_Bodega/Articulo.cs
+++ Proyecto_Bodega/Articulo.cs
@@ -8,7 +8,27 @@ namespace Proyecto_Bodega
 {
     class Articulo
     {
-        public int id { set; get; }
-        public int hola;
-}
+        public int Id { set; get;}
+        public string Descripcion { set; get; }
+        public string Marca { set; get; }
+        public int Costo { set; get; }
+        public int Precio_Unit { set; get; }
+        public string Proveedor { set; get; }
+
+        public Articulo()
+        {
+
+        }
+        public Articulo(int id, string descripcion, string marca, int costo, int precio_unit,)
+        {
+            this.Id = id;
+            this.Descripcion = descripcion;
+            this.Marca = marca;
+            this.Costo = costo;
+            this.Precio_Unit = precio_unit;
+            Proveedor proveedor = new Proveedor();
+            
+
+        }
+    }
 }
