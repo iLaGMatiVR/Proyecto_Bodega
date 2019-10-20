5a4fcc241ce0a493e2b5d25072fb6a8a5463a916
KalilKevin <kalilmaldonado.km@gmail.com>
2019-10-20 15:22:21 -0300

    asd

--- Proyecto_Bodega/Articulo.cs
+++ Proyecto_Bodega/Articulo.cs
@@ -13,22 +13,18 @@ namespace Proyecto_Bodega
         public string Marca { set; get; }
         public int Costo { set; get; }
         public int Precio_Unit { set; get; }
-        public string Proveedor { set; get; }
+        public Proveedor Proveedor { set; get; }
+        public static List<Articulo> ListaArticulos = new List<Articulo>();
 
         public Articulo()
         {
-
-        }
-        public Articulo(int id, string descripcion, string marca, int costo, int precio_unit,)
-        {
-            this.Id = id;
-            this.Descripcion = descripcion;
-            this.Marca = marca;
-            this.Costo = costo;
-            this.Precio_Unit = precio_unit;
-            Proveedor proveedor = new Proveedor();
             
+        }
+        
 
+        public static void AgregarArticulo(Articulo a)
+        {
+            ListaArticulos.Add(a);
         }
     }
 }
