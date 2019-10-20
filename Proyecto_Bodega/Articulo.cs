e56ee3acda2ea08c8160d30426fbf8de7e4dd674
KalilKevin <kalilmaldonado.km@gmail.com>
2019-10-20 20:21:57 -0300

    Agregando Form Articulo

--- Proyecto_Bodega/Articulo.cs
+++ Proyecto_Bodega/Articulo.cs
@@ -8,25 +8,41 @@ namespace Proyecto_Bodega
 {
     class Articulo
     {
-        public int Id { set; get;}
+        public int Id { set; get; }
         public string Descripcion { set; get; }
         public string Marca { set; get; }
-        public int Costo { set; get; }
-        public int Precio_Unit { set; get; }
+        public double Costo { set; get; }
+        public double Precio_Unit { set; get; }
+        public DateTime Fecha_Venc { set; get; }
         public Proveedor Proveedor { set; get; }
         public static List<Articulo> ListaArticulos = new List<Articulo>();
 
-        public Articulo()
+        public override string ToString()
         {
-            
+            return Descripcion;
         }
-        
 
+        //metodo para obtener articulo
+        public static List<Articulo> ObtenerArticulos()
+        {
+            return ListaArticulos;
+        }
+
+        //metodo para agregar articulo
         public static void AgregarArticulo(Articulo a)
         {
             ListaArticulos.Add(a);
         }
 
-        public void ActualizarPrecio () {}
+        //Metodo para eliminar articulo
+        public static void EliminarArticulo(Articulo a)
+        {
+            ListaArticulos.Remove(a);
+        }
+
+        //metodo para modificar articulo
+        public static void ModificarArticulo(Articulo a, int indice)
+        {
+            Articulo.ListaArticulos[indice] = a;
+        }
     }
-}
