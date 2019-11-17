094b202882a43c9c98bb8fb18c1c50eeba364601
GustavoEtcheverry <gustavo.etcheverry@gmail.com>
2019-11-17 00:31:27 -0300

    Modificacion en el campo devolucion

--- Interfaz_Aguila_Curda/frmDevolucion.Designer.cs
+++ Interfaz_Aguila_Curda/frmDevolucion.Designer.cs
@@ -29,130 +29,135 @@
         private void InitializeComponent()
         {
             this.lblTitulo = new System.Windows.Forms.Label();
-            this.btnCancelar = new System.Windows.Forms.Button();
-            this.btnAceptar = new System.Windows.Forms.Button();
-            this.txtPrecioVenta = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.dtgDetalleDevolucion = new System.Windows.Forms.DataGridView();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.cmbDescripcion = new System.Windows.Forms.ComboBox();
             this.dtmFechaDevolucion = new System.Windows.Forms.DateTimePicker();
-            this.txtDescripcionDelProducto = new System.Windows.Forms.TextBox();
             this.lblDescripcionDelProducto = new System.Windows.Forms.Label();
             this.lblFechaDevolucion = new System.Windows.Forms.Label();
             this.txtMotivoDevolucion = new System.Windows.Forms.TextBox();
             this.lblMotivoDevolucion = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgDetalleDevolucion)).BeginInit();
             this.SuspendLayout();
             // 
             // lblTitulo
             // 
             this.lblTitulo.AutoSize = true;
             this.lblTitulo.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.lblTitulo.Font = new System.Drawing.Font("Modern No. 20", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTitulo.Location = new System.Drawing.Point(146, 36);
             this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Size = new System.Drawing.Size(148, 29);
+            this.lblTitulo.Size = new System.Drawing.Size(159, 31);
             this.lblTitulo.TabIndex = 42;
             this.lblTitulo.Text = "Devolucion";
             // 
-            // btnCancelar
+            // btnGuardar
             // 
-            this.btnCancelar.Location = new System.Drawing.Point(275, 337);
-            this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(87, 30);
-            this.btnCancelar.TabIndex = 41;
-            this.btnCancelar.Text = "Cancelar";
-            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Location = new System.Drawing.Point(418, 440);
+            this.btnGuardar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(66, 27);
+            this.btnGuardar.TabIndex = 79;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
             // 
-            // btnAceptar
+            // dtgDetalleDevolucion
             // 
-            this.btnAceptar.Location = new System.Drawing.Point(107, 337);
-            this.btnAceptar.Name = "btnAceptar";
-            this.btnAceptar.Size = new System.Drawing.Size(87, 30);
-            this.btnAceptar.TabIndex = 40;
-            this.btnAceptar.Text = "Aceptar";
-            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.dtgDetalleDevolucion.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgDetalleDevolucion.Location = new System.Drawing.Point(12, 311);
+            this.dtgDetalleDevolucion.Name = "dtgDetalleDevolucion";
+            this.dtgDetalleDevolucion.Size = new System.Drawing.Size(454, 106);
+            this.dtgDetalleDevolucion.TabIndex = 78;
             // 
-            // txtPrecioVenta
+            // btnEliminar
             // 
-            this.txtPrecioVenta.Location = new System.Drawing.Point(232, 273);
-            this.txtPrecioVenta.Name = "txtPrecioVenta";
-            this.txtPrecioVenta.Size = new System.Drawing.Size(130, 20);
-            this.txtPrecioVenta.TabIndex = 39;
+            this.btnEliminar.Location = new System.Drawing.Point(397, 258);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(87, 30);
+            this.btnEliminar.TabIndex = 77;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // btnAgregar
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(86, 273);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 13);
-            this.label1.TabIndex = 38;
-            this.label1.Text = "PrecioVenta";
+            this.btnAgregar.Location = new System.Drawing.Point(277, 258);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(87, 30);
+            this.btnAgregar.TabIndex = 76;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // cmbDescripcion
+            // 
+            this.cmbDescripcion.FormattingEnabled = true;
+            this.cmbDescripcion.Location = new System.Drawing.Point(170, 201);
+            this.cmbDescripcion.Name = "cmbDescripcion";
+            this.cmbDescripcion.Size = new System.Drawing.Size(130, 21);
+            this.cmbDescripcion.TabIndex = 85;
             // 
             // dtmFechaDevolucion
             // 
             this.dtmFechaDevolucion.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtmFechaDevolucion.Location = new System.Drawing.Point(232, 157);
+            this.dtmFechaDevolucion.Location = new System.Drawing.Point(170, 149);
             this.dtmFechaDevolucion.Name = "dtmFechaDevolucion";
             this.dtmFechaDevolucion.Size = new System.Drawing.Size(130, 20);
-            this.dtmFechaDevolucion.TabIndex = 37;
-            // 
-            // txtDescripcionDelProducto
-            // 
-            this.txtDescripcionDelProducto.Location = new System.Drawing.Point(232, 212);
-            this.txtDescripcionDelProducto.Name = "txtDescripcionDelProducto";
-            this.txtDescripcionDelProducto.Size = new System.Drawing.Size(130, 20);
-            this.txtDescripcionDelProducto.TabIndex = 36;
+            this.dtmFechaDevolucion.TabIndex = 84;
             // 
             // lblDescripcionDelProducto
             // 
             this.lblDescripcionDelProducto.AutoSize = true;
-            this.lblDescripcionDelProducto.Location = new System.Drawing.Point(85, 212);
+            this.lblDescripcionDelProducto.Location = new System.Drawing.Point(38, 204);
             this.lblDescripcionDelProducto.Name = "lblDescripcionDelProducto";
             this.lblDescripcionDelProducto.Size = new System.Drawing.Size(126, 13);
-            this.lblDescripcionDelProducto.TabIndex = 35;
+            this.lblDescripcionDelProducto.TabIndex = 83;
             this.lblDescripcionDelProducto.Text = "Descripcion del Producto";
             // 
             // lblFechaDevolucion
             // 
             this.lblFechaDevolucion.AutoSize = true;
-            this.lblFechaDevolucion.Location = new System.Drawing.Point(85, 157);
+            this.lblFechaDevolucion.Location = new System.Drawing.Point(38, 149);
             this.lblFechaDevolucion.Name = "lblFechaDevolucion";
             this.lblFechaDevolucion.Size = new System.Drawing.Size(94, 13);
-            this.lblFechaDevolucion.TabIndex = 34;
+            this.lblFechaDevolucion.TabIndex = 82;
             this.lblFechaDevolucion.Text = "Fecha Devolucion";
             // 
             // txtMotivoDevolucion
             // 
-            this.txtMotivoDevolucion.Location = new System.Drawing.Point(232, 110);
+            this.txtMotivoDevolucion.Location = new System.Drawing.Point(170, 102);
             this.txtMotivoDevolucion.Name = "txtMotivoDevolucion";
             this.txtMotivoDevolucion.Size = new System.Drawing.Size(130, 20);
-            this.txtMotivoDevolucion.TabIndex = 33;
+            this.txtMotivoDevolucion.TabIndex = 81;
             // 
             // lblMotivoDevolucion
             // 
             this.lblMotivoDevolucion.AutoSize = true;
-            this.lblMotivoDevolucion.Location = new System.Drawing.Point(86, 113);
+            this.lblMotivoDevolucion.Location = new System.Drawing.Point(39, 105);
             this.lblMotivoDevolucion.Name = "lblMotivoDevolucion";
-            this.lblMotivoDevolucion.Size = new System.Drawing.Size(93, 13);
-            this.lblMotivoDevolucion.TabIndex = 32;
-            this.lblMotivoDevolucion.Text = "MotivoDevolucion";
+            this.lblMotivoDevolucion.Size = new System.Drawing.Size(96, 13);
+            this.lblMotivoDevolucion.TabIndex = 80;
+            this.lblMotivoDevolucion.Text = "Motivo Devolucion";
             // 
             // frmDevolucion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(510, 433);
-            this.Controls.Add(this.lblTitulo);
-            this.Controls.Add(this.btnCancelar);
-            this.Controls.Add(this.btnAceptar);
-            this.Controls.Add(this.txtPrecioVenta);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(514, 537);
+            this.Controls.Add(this.cmbDescripcion);
             this.Controls.Add(this.dtmFechaDevolucion);
-            this.Controls.Add(this.txtDescripcionDelProducto);
             this.Controls.Add(this.lblDescripcionDelProducto);
             this.Controls.Add(this.lblFechaDevolucion);
             this.Controls.Add(this.txtMotivoDevolucion);
             this.Controls.Add(this.lblMotivoDevolucion);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.dtgDetalleDevolucion);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.lblTitulo);
             this.Name = "frmDevolucion";
             this.Text = "frmDevolucion";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgDetalleDevolucion)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -161,12 +166,12 @@
         #endregion
 
         private System.Windows.Forms.Label lblTitulo;
-        private System.Windows.Forms.Button btnCancelar;
-        private System.Windows.Forms.Button btnAceptar;
-        private System.Windows.Forms.TextBox txtPrecioVenta;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.DataGridView dtgDetalleDevolucion;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.ComboBox cmbDescripcion;
         private System.Windows.Forms.DateTimePicker dtmFechaDevolucion;
-        private System.Windows.Forms.TextBox txtDescripcionDelProducto;
         private System.Windows.Forms.Label lblDescripcionDelProducto;
         private System.Windows.Forms.Label lblFechaDevolucion;
         private System.Windows.Forms.TextBox txtMotivoDevolucion;
--- Proyecto_Bodega/Devolucion.cs
+++ Proyecto_Bodega/Devolucion.cs
@@ -3,18 +3,124 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Proyecto_Bodega
 {
-    class Devolucion
+    public class Devolucion
     {
+        public int Id { set; get; }
         public string Motivo_Devolucion { get; set; }
-
-        public string Descripcion_Producto { set; get; }
-
-        public string Precio_Venta { set; get; }
-
-
+        public Articulo Descripcion { set; get; }
         public DateTime FechaDevolucion { get; set; }
+
+        public static List<Devolucion> listaDevoluciones = new List<Devolucion>();
+
+        public static void AgregarDevolucion(Devolucion d)
+        {
+            using (SqlConnection con = new SqlConnection(SqlServer.CADENA_CONEXION))
+
+            {
+                con.Open();
+                string textoCmd = "INSERT INTO Devolucion (Motivo_Devolucion, Descripcion, FechaDevol)VALUES (@Motivo_Devolucion, @Descripcion,@FechaDevolucion)";
+                SqlCommand cmd = new SqlCommand(textoCmd, con);
+                cmd = d.ObtenerParametros(cmd);
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public static void EliminarDevolucion(Devolucion devolucion)
+        {
+            using (SqlConnection con = new SqlConnection(SqlServer.CADENA_CONEXION))
+
+            {
+                con.Open();
+                string SENTENCIA_SQL = "delete from Devolucion where Id = @Id";
+
+                SqlCommand cmd = new SqlCommand(SENTENCIA_SQL, con);
+                SqlParameter p1 = new SqlParameter("@Id", devolucion.Id);
+                p1.SqlDbType = SqlDbType.Int;
+                cmd.Parameters.Add(p1);
+
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+
+
+
+        public static List<Devolucion> ObtenerDevoluciones()
+        {
+            Devolucion devolucion;
+
+            listaDevoluciones.Clear();
+
+            using (SqlConnection con = new SqlConnection(SqlServer.CADENA_CONEXION))
+            {
+                con.Open();
+                string tectoCMD = "select * from Devolucion";
+                SqlCommand cmd = new SqlCommand(tectoCMD, con);
+
+                SqlDataReader elLectorDeDatos = cmd.ExecuteReader();
+
+                while (elLectorDeDatos.Read())
+                {
+                    devolucion = new Devolucion();
+                    devolucion.Id = elLectorDeDatos.GetInt32(0);
+                    devolucion.Motivo_Devolucion = elLectorDeDatos.GetString(1);
+                    devolucion.articulo = Articulo.ObtenerArticulos(elLectorDeDatos.GetInt32(2));
+                    devolucion.FechaDevolucion = elLectorDeDatos.GetDateTime(3);
+
+                    listaDevoluciones.Add(devolucion);
+
+                }
+            }
+
+            return listaDevoluciones;
+        }
+
+
+
+        public override string ToString()
+        {
+            return this.Motivo_Devolucion;
+        }
+
+
+
+        private SqlCommand ObtenerParametros(SqlCommand cmd, Boolean id = false)
+
+        {
+            SqlParameter p1 = new SqlParameter("@Motivo", this.Motivo_Devolucion);
+            SqlParameter p2 = new SqlParameter("@Descripcion", this.Descripcion.Id);
+            SqlParameter p3 = new SqlParameter("@FechaDevol", this.FechaDevolucion);
+
+            p1.SqlDbType = SqlDbType.VarChar;
+            p2.SqlDbType = SqlDbType.Int;
+            p3.SqlDbType = SqlDbType.DateTime;
+
+            cmd.Parameters.Add(p1);
+            cmd.Parameters.Add(p2);
+            cmd.Parameters.Add(p3);
+
+            if (id == true)
+            {
+                cmd = ObtenerParametrosId(cmd);
+            }
+            return cmd;
+
+        }
+
+
+        private SqlCommand ObtenerParametrosId(SqlCommand cmd)
+        {
+
+            SqlParameter p4 = new SqlParameter("@id", this.Id);
+            p4.SqlDbType = SqlDbType.Int;
+            cmd.Parameters.Add(p4);
+            return cmd;
+        }
     }
+
 }
